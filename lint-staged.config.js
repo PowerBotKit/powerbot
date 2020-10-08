9fed379ee902e98ef4d35d19c88fc49c831fbd8a
Arthur Ming <guobin.ming@pwc.com>
2020-10-08 19:11:17 +0800

    build(core): add core package

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -6,14 +6,18 @@ module.exports = {
 			filenames.length > 10
 				? 'yarn lint'
 				: `tslint --format verbose ${filenames.join(' ')}`;
-		const prettier = `prettier --parser=typescript --write -- ${filenames.join(
-			' '
-		)}`;
+		const prettier =
+			filenames.length > 10
+				? 'prettier --parser=typescript --write'
+				: `prettier --parser=typescript --write -- ${filenames.join(' ')}`;
 		const git = `git add ${filenames.join(' ')}`;
 		return [tslint, prettier, git];
 	},
 	'*.{md,json,js}': filenames => {
-		const prettier = `prettier --write -- ${filenames.join(' ')}`;
+		const prettier =
+			filenames.length > 10
+				? 'prettier --write'
+				: `prettier --write -- ${filenames.join(' ')}`;
 		const git = `git add ${filenames.join(' ')}`;
 		return [prettier, git];
 	}
---
+++ packages/core/__tests__/core.spec.ts
@@ -0,0 +1,5 @@
+import * as core from '../lib/core';
+
+describe('core', () => {
+	it('needs tests');
+});
