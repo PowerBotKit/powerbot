c672369ae096025b6107fc04ef9d56132528e3a4
Arthur Ming <guobin.ming@pwc.com>
2020-10-23 23:38:11 +0800

    ci: add lint script based on tslint and prettier

--- husky.config.js
+++ husky.config.js
@@ -2,7 +2,7 @@ const tasks = arr => arr.join(' && ');
 
 module.exports = {
 	hooks: {
-		'pre-push': tasks(['yarn audit']),
+		'pre-push': tasks(['yarn tslint', 'yarn audit']),
 		'pre-commit': tasks(['ls-lint', 'lint-staged']),
 		'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
 	}
