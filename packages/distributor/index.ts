66d7930e975355ab328f08c5b4b899a4432f2bf7
Jeremy Bao <bao1018@gmail.com>
2021-07-24 11:56:01 +0800

    fix: export more interfaces to external to allow implment

--- packages/distributor/index.ts
+++ packages/distributor/index.ts
@@ -17,5 +17,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
+export * from './lib/cache/index';
+export * from './lib/models/index';
 export * from './lib/server/index';
