5bc01e0850a0f71a3fc493e9335e729daf092a7e
Arthur Ming <guobin.ming@pwc.com>
2021-08-24 20:38:03 +0800

    build: the JsonIntent import path fix

--- packages/consumer/lib/router/index.ts
+++ packages/consumer/lib/router/index.ts
@@ -1,4 +1,3 @@
-import { JsonIntent } from './../../../core/lib/intent/json-intent';
 // Copyright (c) 2020-present PowerBotKit Team
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -27,6 +26,7 @@ import {
 	Intent,
 	IntentYAMLConfig,
 	IntentYAMLWildcardConfig,
+	JsonIntent,
 	WildcardIntent
 } from '@powerbotkit/core';
 
