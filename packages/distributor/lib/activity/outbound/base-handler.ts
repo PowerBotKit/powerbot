71abdb777b76aa5c70613daa85b9f17a39c48494
Arthur Ming <guobin.ming@pwc.com>
2021-09-13 18:39:02 +0800

    fix: chache unlock key should be dialogkey

--- packages/distributor/lib/activity/outbound/base-handler.ts
+++ packages/distributor/lib/activity/outbound/base-handler.ts
@@ -104,7 +104,7 @@ export class OutboundHandlerBase {
 				}
 				const dialogKey = DialogUtil.getDialogKey(dialog.id);
 				await cache.set(dialogKey, dialog, 60 * 60 * 24);
-				await cache.unlock(dialog.id);
+				await cache.unlock(dialogKey);
 			}
 		);
 	}
